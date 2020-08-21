9c8a9bf8ef433c2ba6f0d5aad7ad2fbb5c7b7446
Shubham Baliyan <shubham.baliyan204@gmail.com>
2020-08-21 10:23:54 +0530

    initial commit

---
+++ app/components/Loading.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+const styles = {
+  content: {
+    fontSize: "35px",
+    position: "absolute",
+    left: 0,
+    right: 0,
+    textAlign: "center",
+    marginTop: "20px",
+  },
+};
+export default class Loading extends React.Component {
+  state = { content: this.props.text };
+  componentDidMount() {
+    const { text, speed } = this.props;
+    this.interval = window.setInterval(() => {
+      this.state.content === text + "..."
+        ? this.setState({ content: text })
+        : this.setState(({ content }) => ({ content: content + "." }));
+    }, speed);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return <p style={styles.content}>{this.state.content}</p>;
+  }
+}
+Loading.propTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired,
+};
+Loading.defaultProps = {
+  text: "Loading",
+  speed: 300,
+};
---
+++ app/components/Nav.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { ThemeConsumer } from "../contexts/theme";
+import { NavLink } from "react-router-dom";
+const activeStyle = {
+  color: "rgb(187,46,31)",
+};
+export default function Nav() {
+  return (
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink
+                to="/"
+                className="nav-link"
+                exact
+                activeStyle={activeStyle}
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/battle"
+                className="nav-link"
+                activeStyle={activeStyle}
+              >
+                Battle
+              </NavLink>
+            </li>
+          </ul>
+          <button
+            className="btn-clear"
+            style={{ fontSize: 30 }}
+            onClick={toggleTheme}
+          >
+            {theme === "light" ? "🔦" : "💡"}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  );
+}
