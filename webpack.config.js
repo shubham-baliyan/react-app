d09a5234685da3cbd440430835e2dab281147919
Shubham Baliyan <shubham.baliyan204@gmail.com>
2020-08-21 10:59:33 +0530

    initail commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./app/index.js",
   output: {
@@ -15,12 +15,13 @@ module.exports = {
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
-  mode: "production",
-  //  process.env.NODE_ENV === "production" ? "production" : "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "app/index.html",
     }),
+    new CopyPlugin({ patterns: [{ from: "_redirects" }] }),
   ],
   devServer: {
     historyApiFallback: true,
